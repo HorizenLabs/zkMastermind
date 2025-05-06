4129abdc1e3955824db2f1179a5f4e83c8709d2a
Jofra Montesdeoca <36055057+jmontesdeocanuez@users.noreply.github.com>
2025-05-06 17:54:56 +0100

    feat: add Google Analytics integration (#9)
    * feat: add Google Analytics integration
    
    * refactor: abstract GA ID to env variable

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,16 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import type { AppProps } from "next/app";
-import theme from "../config/theme";
-import GameProvider from "../context/GameContext";
 import { AccountProvider } from "../context/AccountContext";
-
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import GameProvider from "../context/GameContext";
+import GoogleAnalyticsTag from "../components/GoogleAnalyticsTag";
+import theme from "../config/theme";
 export default function App({ Component, pageProps }: AppProps) {
   return (
-      <ChakraProvider theme={theme}>
-        <AccountProvider>
-            <GameProvider>
-              <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <GoogleAnalyticsTag />
+      <AccountProvider>
+        <GameProvider>
+          <Component {...pageProps} />
             </GameProvider>
         </AccountProvider>
       </ChakraProvider>
